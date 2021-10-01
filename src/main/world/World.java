0311a7745b756596b45fd4329f8df9d169aa6ef6
blakebabb <leopard50x1@gmail.com>
2021-10-01 13:39:21 -0700

    added temp ground

--- src/main/world/World.java
+++ src/main/world/World.java
@@ -22,6 +22,9 @@ public class World {
         for (int y = 0; y <= BOARD_SIZE.y / TILE_SIZE; y++) {
             for (int x = 0; x <= BOARD_SIZE.x / TILE_SIZE; x++) {
                 TILEMAP.add(new Tile(p, new PVector(x * TILE_SIZE, y * TILE_SIZE), TILEMAP.size()), x, y);
+                if (y > (BOARD_SIZE.y - 200) / TILE_SIZE) {
+                    TILEMAP.get(x, y).setBase("groundBa_TL");
+                }
             }
         }
 
