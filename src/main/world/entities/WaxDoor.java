1e2a5354186c5e612f26360532dd96772b02897b
blakebabb <leopard50x1@gmail.com>
2021-10-02 21:00:31 -0700

    wax adds particles as it goes down

--- src/main/world/entities/WaxDoor.java
+++ src/main/world/entities/WaxDoor.java
@@ -1,9 +1,12 @@
 package main.world.entities;
 
 import main.Main;
+import main.particles.GravityParticle;
 import main.world.World;
 import processing.core.PApplet;
 import processing.core.PVector;
+
+import java.awt.*;
 
 public class WaxDoor extends Door {
     public WaxDoor(PApplet p, World world, PVector position, PVector moveDir, float speed) {
@@ -16,6 +19,12 @@ public class WaxDoor extends Door {
     public void update() {
         if (onFire) {
             closed = false;
+        }
+        if (!closed && P.random(2) < 1) {
+            world.behindParticles.add(new GravityParticle(P,
+                    P.random(position.x, position.x + WIDTH),
+                    P.random(position.y, position.y + HEIGHT),
+                    new Color(250, 255, 180), world.behindParticles));
         }
         super.update();
     }
