e71691bbfa0d49638b3d85fe46033396ae3e13f0
blakebabb <leopard50x1@gmail.com>
2021-10-03 15:20:14 -0700

    sprited spikes

--- src/main/world/entities/Spikes.java
+++ src/main/world/entities/Spikes.java
@@ -4,9 +4,12 @@ import main.Main;
 import main.misc.CollisionBox;
 import main.world.World;
 import processing.core.PApplet;
+import processing.core.PImage;
 import processing.core.PVector;
 
 public class Spikes extends Entity {
+
+    private final PImage sprite;
 
     public Spikes(PApplet p, World world, PVector position) {
         super(p, world, new CollisionBox(
@@ -14,11 +17,12 @@ public class Spikes extends Entity {
                 new PVector(0, -10),
                 new PVector(Main.TILE_SIZE, 10)
         ), position);
+
+        sprite = Main.sprites.get("spikes");
     }
 
     @Override
     public void draw() {
-        P.fill(255, 0, 0);
-        P.rect(position.x, position.y - 10, Main.TILE_SIZE, 10);
+        P.image(sprite, position.x, position.y - Main.TILE_SIZE, Main.TILE_SIZE, Main.TILE_SIZE);
     }
 }
